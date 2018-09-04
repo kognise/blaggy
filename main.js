fe0f28c665b14447f6d462a30f4bcc831127715f
Felix Mattick <felix.mattick@gmail.com>
2018-09-03 23:49:45 -0400

    Fix testing and make css generatable

--- main.js
+++ main.js
@@ -1,10 +1,47 @@
 var express = require("express");
-var app = express();
-app.get("/", function (req, res) {
-  res.status(200).send("ok");
-});
-var server = app.listen(3000, function () {
-  var port = server.address().port;
-  console.log("Listening at port %s.", port);
-});
-module.exports = server;
+var sass = require("node-sass");
+var ora = require("ora");
+
+var serverExport;
+
+var generatedCSS = "";
+var generateCSSSpinner = ora("Generating CSS");
+var generateCSSPromise = new Promise(function(resolve, reject) {
+  generateCSSSpinner.start();
+  try {
+    generatedCSS = sass.renderSync({ file: "styles/main.css" }).css.toString("utf8");
+    resolve();
+  } catch(error) {
+    reject();
+  }
+}).then(
+  () => {
+    generateCSSSpinner.succeed();
+    startTheSite();
+  },
+  () => {
+    generateCSSSpinner.fail();
+    process.exit(1);
+  }
+);
+
+function startTheSite() {
+  var app = express();
+
+  app.get("/", function (req, res) {
+    res.status(200).send("ok");
+  });
+
+  app.get("/stylesheet.css", function (req, res) {
+    res.status(200).send(generatedCSS);
+  });
+
+  var server = app.listen(3000, function () {
+    var port = server.address().port;
+    console.log("Listening at port %s.", port);
+  });
+
+  serverExport = server;
+}
+
+module.exports = serverExport;
